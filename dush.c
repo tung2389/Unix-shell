35376e19c0aee6061de89fcaf5a37382da46bc43
Tung Luu <luukhanhtung2002@gmail.com>
2022-09-11 15:50:33 -0400

    Update parsing and validation logic to handle edge cases and redirection

--- dush.c
+++ dush.c
@@ -19,7 +19,7 @@
 - print a message when syntax error
 */
 
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
@@ -67,6 +67,7 @@ int main(int argc, char *argv[]) {
         }
         
         char *fullCmd = NULL;
+        int cntCmd = 0;
         while ((fullCmd = strsep(&line, "&")) != NULL) {
             char **args = NULL;
             /*
@@ -74,45 +75,144 @@ int main(int argc, char *argv[]) {
             - if cmd is a valid exit, exit(0)
             - otherwise, spawn new process to handle the new cmd.
             - after that, wait for all to complete.
+            
             */
-            ParserResult parsedCmd = parseCmd(fullCmd);
-            char *cmd = parsedCmd.argv[0];
+            ParserResult parserRes = parseAndValidateCmd(fullCmd);
+            if (!parserRes.isValid) {
+                printError();
+                continue;
+            }
+            char *cmd = parserRes.argv[0];
             if (strcmp(cmd, "exit") == 0) {
-                if (parsedCmd.argc > 1) {
-                    printError(STDERR_FILENO);
+                if (parserRes.argc > 1) {
+                    printError();
                     continue;
                 }
                 exit(0);
             }
+            printf("full cmd: %s\n", fullCmd);
+            printf("main cmd: %s\n", cmd);
+            // executeCmd()
         }
     }
 }
 
-ParserResult parseCmd(char *fullCmd) {
+ParserResult parseAndValidateCmd(char *fullCmd) {
+    /*
+    Edge cases:
+        - Command is null
+        - Command has multiple redirection symbols
+        - Command is redirected to multiple files
+    */ 
     ParserResult result;
+    result.isValid = true;
     result.argc = 0;
+    result.redirection = NULL;
 
-    char *rest = fullCmd;
-    while (strtok_r(rest, " ", &rest)) {
-        result.argc += 1;
+    if (fullCmd == NULL) {
+        result.isValid = false;
+        return result;
     }
+
+    /*
+    Check if command has multiple redirection symbols.
+    */
+    char *copyCmd = mallocStr(fullCmd);
+    char *copyCmdPtr = copyCmd; // we need to keep the original pointer so that we can free the block later 
+    char *savePtr = NULL;   // internal pointer for strtok_r
+
+    int redirSymbolCnt= -1;
+    while (strtok_r(copyCmd, ">", &savePtr)) {
+        redirSymbolCnt += 1;
+        copyCmd = NULL; // Set pointer to NULL for strtok_r to work
+    }
+    free(copyCmdPtr);
+    if (redirSymbolCnt > 1) {
+        result.isValid = false;
+        return result;
+    }
+
+    /*
+    Check if command is redirected to multiple files.
+    */
+    char *redirFile = strchr(fullCmd, '>');
+    if (redirFile != NULL) {
+        redirFile += 1;
+        int redirFileCnt = 0;
+
+        copyCmd = mallocStr(redirFile);
+        copyCmdPtr = copyCmd;
+        savePtr = NULL;  
+        result.redirection = mallocStr(copyCmd);
+        while (strtok_r(copyCmd, " ", &savePtr)) {
+            redirFileCnt += 1;
+            copyCmd = NULL; // Set pointer to NULL for strtok_r to work
+        }
+        free(copyCmdPtr);
+        if (redirFileCnt > 1) {
+            result.isValid = false;
+            return result;
+        }
+    }
+    /*
+    Extract argc
+    */
+    copyCmd = mallocStr(fullCmd);
+    copyCmdPtr = copyCmd;
+    savePtr = NULL;  
+    char *arg;
+    while ((arg = strtok_r(copyCmd, " ", &savePtr))) {
+        if (strcmp(arg, ">") == 0) {
+            break;
+        }
+        result.argc += 1;
+        copyCmd = NULL; // Set pointer to NULL for strtok_r to work
+    }
+    free(copyCmdPtr);
+
+    /*
+    Extract argv
+    */
     result.argv = (char **) malloc(sizeof(char *) * result.argc);
-    rest = fullCmd;
+    copyCmd = mallocStr(fullCmd);
+    copyCmdPtr = copyCmd;
+    savePtr = NULL;  
     for (int i = 0; i < result.argc; i++) {
-        char *arg = strtok_r(rest, " ", &rest);
+        char *arg = strtok_r(copyCmd, " ", &savePtr);
         int argLen = strlen(arg);
         result.argv[i] = mallocStr(arg);
+        copyCmd = NULL; // Set pointer to NULL for strtok_r to work
     }
+    free(copyCmdPtr);
     return result;
 }
 
-// bool executeCmd(char *cmd) {   
-//     if (strcmp(cmd, "exit") {
-//         exit(0);
-//     }
-// }
+void executeCmd(int argc, char **argv) {
+    /*
+    Edge cases:
+        - argc <= 0
+    Logic:
+        - validate each command before running 
+    */
+    if (argc <= 0) {
+        exit(1);
+    }
+    char *cmd = argv[0];
+    if (strcmp(cmd, "cd") == 0) {
+        if (argc <= 1 || argc > 2) {
+            printf("test");
+        }
+    }
+}
 
 char * mallocStr(char *str) {
+    /*
+    Edge cases:
+        - str is NULL
+    */
+    if (str == NULL) {
+        return NULL;
+    }
     int len = strlen(str);
     char *p = malloc(len + 1);
     p[len] = '\0';
@@ -120,7 +220,7 @@ char * mallocStr(char *str) {
     return p;
 }
 
-void printError(int stderr_fd) {
+void printError() {
     char error_message[30] = "An error has occurred\n";
-    write(stderr_fd, error_message, strlen(error_message)); 
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
--- dush.h
+++ dush.h
@@ -1,13 +1,15 @@
 #include <stdbool.h>
 
 typedef struct ParserResult {
-    int argc;
-    char **argv;
+    bool isValid; // Is this command valid 
+    int argc; // Number of arguments in this command (including command name) 
+    char **argv; // Array of arguments of this command
+    char *redirection; // Name of the file that is redirected to
 } ParserResult;
 
 // bool isB
-ParserResult parseCmd(char *fullCmd);
-bool executeCmd(char *cmd);
+ParserResult parseAndValidateCmd(char *fullCmd);
+void executeCmd(int argc, char **argv);
 
 char * mallocStr(char *str);
-void printError(int stderr_fd);
+void printError();
