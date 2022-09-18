72f8e0369b1c7a8d2fd41ea2e855e28d4d9d6395
Duc Doan <doan_d1@denison.edu>
2022-09-18 14:03:48 -0400

    non build in

--- services/executor.c
+++ services/executor.c
@@ -5,9 +5,29 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <dirent.h>
+#include <sys/wait.h>
 #include "executor.h"
 #include "logger.h"
 #include "../common/common.h"
+
+FILE* redirect(char *redirection) {
+    /*
+    Edge cases:
+        - redirection is NULL (no need for redirection)
+    */
+    if (redirection == NULL) {
+        return NULL;
+    }
+    FILE * out = NULL; 
+    out = fopen(redirection, "w");
+    if (out == NULL) {
+        return NULL;
+    }
+    int outFd = fileno(out);
+    dup2(outFd, STDOUT_FILENO);
+    dup2(outFd, STDERR_FILENO);
+    return out;
+}
 
 void executeCmd(int argc, char **argv, char *redirection, int *pathCnt, char ***paths) {
     /*
@@ -64,25 +84,26 @@ void executeCmd(int argc, char **argv, char *redirection, int *pathCnt, char ***
     }
     // Execute non built-in commands
     else {
-
+        argv[argc+1] = NULL;
+        printf("%d", argc);
+        int pid = fork();
+        if (pid<0){
+            printf("Fork fail");
+        }else if(pid == 0){
+            // if (redirection != NULL){
+            //     FILE* r = redirect(redirection);
+            //     execv(cmd, argv);
+            //     // close(STDOUT_FILENO);
+            //     // open(redirection, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+            // }
+            printf("%s", cmd);
+            execv(cmd, argv);
+            printf("Unknown cmd\n");
+            exit(0);
+        }else{
+            int rc_wait = wait(NULL);
+        }
     }
 }
 
-FILE * redirect(char *redirection) {
-    /*
-    Edge cases:
-        - redirection is NULL (no need for redirection)
-    */
-    if (redirection == NULL) {
-        return NULL;
-    }
-    FILE * out = NULL; 
-    out = fopen(redirection, "w");
-    if (out == NULL) {
-        return NULL;
-    }
-    int outFd = fileno(out);
-    dup2(outFd, STDOUT_FILENO);
-    dup2(outFd, STDERR_FILENO);
-    return out;
-}
+
